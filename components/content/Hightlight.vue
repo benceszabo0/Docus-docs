60e39fbee0e3530d913b4dfaccf721f749041399
Szabó István Bence <bence.szabo@levoolabs.com>
2023-10-13 16:38:50 +0200

    docus init

---
+++ components/content/Hightlight.vue
@@ -0,0 +1,14 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="hightlighted">
+        <ContentSlot :use="$slots.default" unwrap="pre" />
+    </div>
+</template>
+
+<style scoped>
+.hightlighted {
+    color: var(--color-blue-500);
+    font-size: 1.5rem;
+}
+</style>
